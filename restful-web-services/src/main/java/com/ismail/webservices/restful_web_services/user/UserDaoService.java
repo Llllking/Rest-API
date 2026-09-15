55b59f673a65982dbf346b44f2a19ef0415a8778
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-09-15 11:25:40 +0100

    make User class and UserDaoService that have a static list to assimilate a database and have some methods that interact with the static list

---
+++ restful-web-services/src/main/java/com/ismail/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,27 @@
+package com.ismail.webservices.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public void save(User user){
+        users.add(new User(user.getId(), user.getName(), user.getBirthDate()));
+    }
+
+    public User findOne(int id){
+        return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
+    }
+}
