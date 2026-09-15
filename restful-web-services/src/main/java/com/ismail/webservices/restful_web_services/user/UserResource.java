3938e32b0929462b437ae1846f57737609c692df
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-09-15 15:32:03 +0100

    improve create method now the method create and return URI of the created user and also changed the responseStatus now it shows 201 created instead of 200 success

--- restful-web-services/src/main/java/com/ismail/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/ismail/webservices/restful_web_services/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.ismail.webservices.restful_web_services.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 //A class that exposes User-related REST endpoints.
@@ -25,7 +28,9 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
