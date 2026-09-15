2500decef8ed062b92c7501dab7cbf59fa27409f
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-09-15 17:05:23 +0100

    implementing delete user method

--- restful-web-services/src/main/java/com/ismail/webservices/restful_web_services/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/ismail/webservices/restful_web_services/user/UserDaoService.java
@@ -35,4 +35,8 @@ public class UserDaoService {
     public User findOne(int id){
         return users.stream().filter(user -> user.getId().equals(id)).findFirst().orElse(null);
     }
+
+    public void deleteById(int id) {
+        users.removeIf(user -> user.getId().equals(id));
+    }
 }
